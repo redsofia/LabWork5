be18a67e68d60d5b059479e26b37c6e2420802e5
User <User@USER->
2025-01-28 20:25:01 +0300

    Добавьте файлы проекта.

---
+++ LabWork5/File.cs
@@ -0,0 +1,67 @@
+﻿class File
+{
+    private string _fileName;
+    private string _fullPath;
+    private long _fileSize;
+
+    public File()
+    {
+        _fileName = "default.txt";
+        _fullPath = "C:\\default\\default.txt";
+        _fileSize = 0;
+    }
+
+    public File(string fileName, string fullPath, long fileSize)
+    {
+        FileName = fileName;
+        FullPath = fullPath;
+        FileSize = fileSize;
+    }
+
+    public string FileName
+    {
+        get => _fileName;
+        set
+        {
+            if (value.Length < 1)
+            {
+                Console.WriteLine("Имя файла должно содержать хотя бы 1 символ.");
+            }
+            _fileName = value;
+        }
+    }
+
+    public string FullPath
+    {
+        get => _fullPath;
+        set
+        {
+            if (value.Length < 1)
+            {
+                Console.WriteLine("Полный путь должен содержать хотя бы 1 символ.");
+            }
+            _fullPath = value;
+        }
+    }
+
+    public long FileSize
+    {
+        get => _fileSize;
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Размер файла не может быть отрицательным.");
+            }
+            _fileSize = value;
+        }
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Имя файла: {_fileName}");
+        Console.WriteLine($"Полный путь: {_fullPath}");
+        Console.WriteLine($"Размер файла: {_fileSize} байт");
+        Console.WriteLine();
+    }
+}
---
+++ LabWork5/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main() //string[] args
+    {
+        File defaultFile = new File();
+        Console.WriteLine("Информация о defaultFile:");
+        defaultFile.DisplayInfo();
+
+        File parameterFile = new File("example.txt", "C:\\example\\example.txt", 1024);
+        Console.WriteLine("Информация о parameterFile:");
+        parameterFile.DisplayInfo();
+
+        parameterFile.FileName = ""; // Некорректное значение
+        parameterFile.FullPath = ""; // Некорректное значение
+        parameterFile.FileSize = -10; // Некорректное значение
+
+        parameterFile.FileName = "new_example.txt"; // Корректное значение
+        parameterFile.FullPath = "C:\\example\\new_example.txt"; // Корректное значение
+        parameterFile.FileSize = 2048; // Корректное значение
+
+        Console.WriteLine("Обновленная информация о файле:");
+        parameterFile.DisplayInfo();
+
+        File[] files = new File[]
+        {
+            new File("example.txt", "C:\\example\\example.txt", 1024),
+            new File("image.png", "C:\\images\\image.png", 512),
+            new File("summary.txt", "C:\\documents\\reports.txt", 256),
+            new File("notes.txt", "C:\\notes\\lists.txt", 128)
+        };
+
+        string searchFileName = "example.txt";
+        long searchFileSize = 500;
+
+        Console.WriteLine("Файлы с указанным названием:");
+        foreach (var file in files)
+        {
+            if (file.FileName == searchFileName)
+            {
+                file.DisplayInfo();
+            }
+        }
+
+        Console.WriteLine($"Файлы, размер которых превышает {searchFileSize} байт:");
+        foreach (var file in files)
+        {
+            if (file.FileSize > searchFileSize)
+            {
+                file.DisplayInfo();
+            }
+        }
+
+    }
+}
+
